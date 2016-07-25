d9a8c5167945bd80d6236fe94a44820a09f70ca7
Alex Beals <alexcbeals@gmail.com>
2016-07-25 01:17:07 -0400

    added header image, basic CSS

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Groupme Analysis</title>
+
+		<link type="text/css" rel="stylesheet" href="build/home.css" />
+	</head>
+	<body>
+		<header>
+			<img src="assets/header.png" alt="header" />
+		</header>
+	</body>
+</html>
