75cb66661fe53d24374203797b72d19d8d15d3a9
Alex Beals <alexcbeals@gmail.com>
2016-07-25 01:57:28 -0400

    modified CTA button, began setting up JS

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-
+	// Character set adapted from http://ux.stackexchange.com/a/21078
+	$code = substr(str_shuffle(str_repeat("abcdefghkmnoprstwxzABCDEFGHJKLMNPQRTWXY", 6)), 0, 6);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,6 +8,8 @@
 		<title>Groupme Analysis</title>
 
 		<link type="text/css" rel="stylesheet" href="build/home.css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<script src="js/main.js"></script>
 	</head>
 	<body>
 		<header>
@@ -31,14 +34,14 @@
 					<h3>Step 3</h3>
 					Set the user's name to:
 					<div class="well">
-						ZcJasF
+						<?php echo $code; ?>
 					</div>
 					<img src="assets/add2.jpg" />
 				</li>
 				<li>
 					<h3>Step 4</h3>
 					Click this button once the user has been added to the group!
-					<button type="submit">Added!</button>
+					<button type="submit" class="cta" data-name="<?php echo $code; ?>">Added!</button>
 				</li>
 			</ul>
 		</div>
