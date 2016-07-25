00f7bffd4a70d80ddb1feed1e229bf92c1d9be44
Alex Beals <alexcbeals@gmail.com>
2016-07-25 01:46:28 -0400

    adding in basic helpful images, working on steps outline

--- index.php
+++ index.php
@@ -12,5 +12,35 @@
 		<header>
 			<img src="assets/header.png" alt="header" />
 		</header>
+		<div class="main">
+			<ul class="steps">
+				<li>
+					<h3>Step 1</h3>
+					Add user from menu.
+				</li>
+				<li>
+					<h3>Step 2</h3>
+					Use the phone number:
+					<div class="well">
+						+1 619-432-4317
+					</div>
+					and click 'Add new phone number'
+					<img src="assets/add1.jpg" />
+				</li>
+				<li>
+					<h3>Step 3</h3>
+					Set the user's name to:
+					<div class="well">
+						ZcJasF
+					</div>
+					<img src="assets/add2.jpg" />
+				</li>
+				<li>
+					<h3>Step 4</h3>
+					Click this button once the user has been added to the group!
+					<button type="submit">Added!</button>
+				</li>
+			</ul>
+		</div>
 	</body>
 </html>
