c2396fa53130d1846f45467f50b2b831a428b1e4
Alex Beals <alexcbeals@gmail.com>
2016-08-10 02:55:16 -0400

    took out error printing

--- php/groupme.php
+++ php/groupme.php
@@ -1,6 +1,6 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
+	// error_reporting(E_ALL);
+	// ini_set('display_errors', 1);
 
 	if (!defined('API')) {
 		die('Direct access not permitted.');
